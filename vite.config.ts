09f2c8c1f00dbcfa307341f98fb435e4c93789f5
Gomes027 <143885905+Gomes027@users.noreply.github.com>
2025-07-05 21:33:00 -0300

    feat: corrigindo vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/movieapp/',
+  base: '/moviapp_unisales/',
   plugins: [react()],
 })
